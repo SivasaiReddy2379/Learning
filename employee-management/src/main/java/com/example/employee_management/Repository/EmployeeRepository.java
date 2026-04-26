f5b8150bbdf1dbe2d098b96a1525fb6ab32e364c
SivasaiReddy2379 <sivasaireddy2112@gmail.com>
2026-04-26 19:11:47 +0530

    Add files via upload

---
+++ employee-management/src/main/java/com/example/employee_management/Repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.example.employee_management.Repository;
+
+import com.example.employee_management.Entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
