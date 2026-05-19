aea2f5ba0a72fae5d5641db04438ff94a64ffdd6
Ubuntu <ubuntu@ip-172-31-87-38.ec2.internal>
2026-05-19 18:44:45 +0000

    test

--- Learning/employee-management/src/main/java/com/example/employee_management/Repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.employee_management.Repository;
-
-import com.example.employee_management.Entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-}
